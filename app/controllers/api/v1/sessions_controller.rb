aa69106830186349597bd6f3287f8006e85815a5
willamy388720 <willamygt@gmail.com>
2020-05-19 11:22:45 -0300

    Adding destroy action to the sessions controller with tests

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -6,11 +6,18 @@ class Api::V1::SessionsController < ApplicationController
     if user && user.valid_password?(session_params[:password])
       sign_in user, store: false
       user.generate_authentication_token!
-      user.save!
+      user.save
       render json: user, status: 200
     else
       render json: { errors: 'Invalid password or email'}, status: 401
     end
+  end
+
+  def destroy
+    user = User.find_by(auth_token: params[:id])
+    user.generate_authentication_token!
+    user.save
+    head 204
   end
 
   private
